1557616158a2dcd02f2a462a9680b1ad0a9c2b4f
Esdeath0210 <scp901129@gmail.com>
2024-09-13 17:57:20 +0800

    Update tt.R

--- tt.R
+++ tt.R
@@ -3,3 +3,5 @@ setwd('C:/Users/Esdeath/Downloads/data')
 library(readxl)
 fish<-read.table('reef_fish.txt', header=T, sep='\t', dec='.')
 barplot(fish$richness, main="Top 10 reef fish Richness (Allen, 2000)", horiz=TRUE, names.arg=fish$country, cex.names=0.5, las=1)
+
+#123
